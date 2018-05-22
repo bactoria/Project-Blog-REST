d9ca4c49998d6e10ecac57ad09fb2d49be4682ed
bactoria <bactoria@gmail.com>
2018-05-23 01:16:17 +0900

    Add category

--- src/test/java/com/bactoria/toy1/domain/PostsRepositoryTest.java
+++ src/test/java/com/bactoria/toy1/domain/PostsRepositoryTest.java
@@ -1,8 +1,13 @@
 package com.bactoria.toy1.domain;
 
+import com.bactoria.toy1.domain.category.Category;
+import com.bactoria.toy1.domain.category.CategoryRepository;
+import com.bactoria.toy1.domain.category.CategorySaveRequestDto;
 import com.bactoria.toy1.domain.post.Post;
 import com.bactoria.toy1.domain.post.PostRepository;
+import com.bactoria.toy1.domain.post.PostSaveRequestDto;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +28,17 @@ public class PostsRepositoryTest {
     @Autowired
     PostRepository postsRepository;
 
-    @After
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    private Category category;
+
+    @Before
+    public void 카테고리_ID1() {
+        category = categoryRepository.getOne((long)1);
+    }
+
+        @After
     public void cleanup(){
         postsRepository.deleteAll();
     }
@@ -31,10 +46,12 @@ public class PostsRepositoryTest {
     @Test
     public void 게시글저장_불러오기() {
 
+
         //given
         postsRepository.save(Post.builder()
                 .title("제목입니다")
                 .content("내용이다내요오오옹")
+                .category(category)
                 .build());
 
         //when
@@ -56,6 +73,7 @@ public class PostsRepositoryTest {
         postsRepository.save(Post.builder()
                 .title("타임등록하자")
                 .content("타임내용이다")
+                .category(category)
                 .build());
 
         //when 리스트 뽑아와
