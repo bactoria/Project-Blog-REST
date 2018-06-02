eddbc760d7d1606d1cca6ce80495ef8be29149cd
bactoria <bactoria@gmail.com>
2018-06-02 21:18:36 +0900

    Add security configuration

---
+++ src/main/java/com/bactoria/toy1/configuration/securityConfig.java
@@ -0,0 +1,25 @@
+package com.bactoria.toy1.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class securityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.httpBasic();
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.DELETE).fullyAuthenticated()
+                .antMatchers(HttpMethod.POST).fullyAuthenticated()
+                .antMatchers(HttpMethod.PUT).fullyAuthenticated()
+                .anyRequest().permitAll();
+
+        http.csrf().disable();
+    }
+
+}
