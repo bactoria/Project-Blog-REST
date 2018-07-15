8cd26ef398849087f0b4ddb46ce6dde13196ef3e
bactoria <bactoria@gmail.com>
2018-07-15 21:53:44 +0900

    Add test code add test002_게시글_추가한다

--- src/test/java/com/bactoria/toy1/web/AdminControllerTest.java
+++ src/test/java/com/bactoria/toy1/web/AdminControllerTest.java
@@ -3,6 +3,9 @@ package com.bactoria.toy1.web;
 import com.bactoria.toy1.domain.category.Category;
 import com.bactoria.toy1.domain.category.CategorySaveRequestDto;
 import com.bactoria.toy1.domain.category.CategoryService;
+import com.bactoria.toy1.domain.post.Post;
+import com.bactoria.toy1.domain.post.PostSaveRequestDto;
+import com.bactoria.toy1.domain.post.PostService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
@@ -32,13 +35,21 @@ public class AdminControllerTest {
     @Mock
     CategoryService categoryServiceMock;
 
+    @Mock
+    PostService postServiceMock;
+
     MockMvc mockMvc;
+
+    Category category;
 
     @Before
     public void setUp() {
         mockMvc = MockMvcBuilders
                 .standaloneSetup(adminController)
                 .build();
+
+        category = Category.builder().name("카테고리1").build();
+
     }
 
     private String jsonStringFromObject(Object object) throws JsonProcessingException {
@@ -53,8 +64,6 @@ public class AdminControllerTest {
 
         String dtoJson = jsonStringFromObject(dto);
 
-        Category category = Category.builder().name("카테고리1").build();
-
         when(categoryServiceMock.saveCategory(any(CategorySaveRequestDto.class))).thenReturn(category);
 
         mockMvc.perform(post("/api/categories")
@@ -66,4 +75,27 @@ public class AdminControllerTest {
                 .andExpect(jsonPath("$.name",is("카테고리1")));
 
     }
+
+    @Test
+    public void test002_게시글_추가한다() throws Exception {
+
+        String title = "제목";
+        String content = "내용";
+
+        PostSaveRequestDto dto = PostSaveRequestDto.builder().content(content).title(title).category(category).build();
+
+        String dtoJson = jsonStringFromObject(dto);
+
+        when(postServiceMock.savePost(any(PostSaveRequestDto.class))).thenReturn( Post.builder().title("제목").content("내용").build() );
+
+        mockMvc.perform(post("/api/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(dtoJson))
+
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.title",is("제목")));
+
+    }
+
 }
