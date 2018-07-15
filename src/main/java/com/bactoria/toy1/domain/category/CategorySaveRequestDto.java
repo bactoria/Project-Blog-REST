8e3efff2619d9aede9a248724f926b7e26a09a1e
bactoria <bactoria@gmail.com>
2018-07-15 22:25:37 +0900

    Remove setter annotation setter annotation doesn't need

--- src/main/java/com/bactoria/toy1/domain/category/CategorySaveRequestDto.java
+++ src/main/java/com/bactoria/toy1/domain/category/CategorySaveRequestDto.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Repository;
 
-@Setter
 @Getter
 @NoArgsConstructor
 public class CategorySaveRequestDto {
