fd6bea20e310b9a514040dc7c8d7694126de8527
bactoria <bactoria@gmail.com>
2018-10-04 01:42:39 +0900

    Add properties for auth

--- src/test/java/com/bactoria/toy1/web/AdminControllerTest.java
+++ src/test/java/com/bactoria/toy1/web/AdminControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = "spring.config.location=classpath:test/application-auth.yml")
 public class AdminControllerTest {
 
     @Autowired
