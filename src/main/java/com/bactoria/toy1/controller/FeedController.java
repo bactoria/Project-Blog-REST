7aff0d51d64081793a84af776eb402f42ac9c2b1
bactoria <bactoria@gmail.com>
2018-10-12 18:17:24 +0900

    Remove cross-origin annotation

--- src/main/java/com/bactoria/toy1/controller/FeedController.java
+++ src/main/java/com/bactoria/toy1/controller/FeedController.java
@@ -5,14 +5,12 @@ import com.rometools.rome.feed.rss.Channel;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@CrossOrigin
 @RequestMapping("/api/feed")
 public class FeedController {
 
