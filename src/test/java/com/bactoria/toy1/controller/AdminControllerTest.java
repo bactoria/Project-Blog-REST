2bc0a0d98f623d4be9da5f1cb2fb1e437f4be62a
bactoria <bactoria@gmail.com>
2018-10-12 18:07:49 +0900

    Edit resource for test

--- src/test/java/com/bactoria/toy1/controller/AdminControllerTest.java
+++ src/test/java/com/bactoria/toy1/controller/AdminControllerTest.java
@@ -12,7 +12,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.properties.PropertyMapping;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -32,7 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(properties = "spring.config.location=classpath:application-auth.yml")
+@TestPropertySource(locations = "classpath:/security-auth.properties")
 public class AdminControllerTest {
 
     @Autowired
